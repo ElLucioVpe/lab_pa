f4109344f663116091d289f1c45de49336f371bf
ZedphX <49252618+ZedphX@users.noreply.github.com>
2019-06-12 10:00:32 -0300

    Usuario agregado

---
+++ classes/headers/Usuario.h
@@ -0,0 +1,26 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <iostream>
+#include <string>
+
+class Usuario {
+    private:
+        std::string NickName;
+        std::string ImgPerfil;
+        std::string Contrasenia;
+    public:
+        Usuario(std::string _nickname, std::string _img, std::string _contra);
+        std::string getNickName();
+        std::string getImgPerfil();
+        std::string getContrasenia();
+
+        void setNickname(std::string _nickname);
+        void setImgPerfil(std::string _img);
+        void setContrasenia(std::string _contra);
+
+        bool contraCorrecta(std::string c);
+        ~Usuario();
+}
+
+#endif /* USUARIO_H */
