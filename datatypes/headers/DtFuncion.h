7ea867af39509787d4205a96f249f9c6882980d5
ZedphX <estebanleivas103@gmail.com>
2019-06-28 19:49:55 -0300

    Me falto este

--- datatypes/headers/DtFuncion.h
+++ datatypes/headers/DtFuncion.h
@@ -6,10 +6,11 @@
 class DtFuncion : public ICollectible {
 public:
     DtFuncion();
-    DtFuncion(int, time_t);
+    DtFuncion(int, time_t, int);
 
     int getIdFuncion() const;
     time_t getHorario() const;
+	int getIdSala();
 	bool operator< (const DtFuncion& f) const;
 
     virtual ~DtFuncion();
@@ -17,6 +18,7 @@ public:
 private:
     int idFuncion;
 	time_t horario;
+	int idSala;
 };
 
 #endif	/* DTFUNCION_H */
